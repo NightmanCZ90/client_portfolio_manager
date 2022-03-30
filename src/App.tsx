505af4664c22c09438240b73d0ac74cb19b5af04
NightmanCZ90 <novak.jan90@gmail.com>
2022-03-30 23:09:47 +0200

    feat: Add basic structure with Home page and SideNavigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import SideNavigation from './components/SideNavigation';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
 
   return (
     <div className="App">
-      Portfolio Manager
+      <SideNavigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/SideNavigation/SideNavigation.tsx
@@ -0,0 +1,9 @@
+const SideNavigation: React.FC = () => {
+  return (
+    <div>
+      SideNavigation
+    </div>
+  )
+}
+
+export default SideNavigation;
