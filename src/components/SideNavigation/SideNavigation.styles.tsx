1eb937b9962a321ba1dae0a6932ccb6b00cd2d70
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-01 00:48:30 +0200

    style: Add basic styles to SideNavigation

---
+++ src/components/SideNavigation/SideNavigation.styles.tsx
@@ -0,0 +1,12 @@
+import { IconButton } from '@mui/material';
+import { styled } from '@mui/system';
+
+export const StyledSideNavigation = styled('div')`
+  background-color: #423c3c;
+  width: 3.5rem;
+  padding: 0.5rem;
+`;
+
+export const NavigationButton = styled(IconButton)`
+  color: #fff;
+`;
