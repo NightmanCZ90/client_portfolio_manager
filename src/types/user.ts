965b0e8df0a66f0b58c61cfd33612c32446867b5
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-02 18:03:06 +0200

    data: Add type for CurrentUser

---
+++ src/types/user.ts
@@ -0,0 +1,17 @@
+export enum Role {
+  Investor = 'investor',
+  PortfolioManager = 'portfolioManager',
+  Administrator = 'administrator',
+}
+
+export interface CurrentUser {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  role: Role;
+  isActive: boolean;
+}
