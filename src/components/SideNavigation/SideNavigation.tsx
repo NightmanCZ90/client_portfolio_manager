c828eeb95aaf728d6e8770a1a3bf89589f339557
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-09 15:23:43 +0200

    refactor: Update SideNavigation buttons rendering

--- src/components/SideNavigation/SideNavigation.tsx
+++ src/components/SideNavigation/SideNavigation.tsx
@@ -1,4 +1,5 @@
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
 import DataSaverOffIcon from '@mui/icons-material/DataSaverOff';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Tooltip } from '@mui/material';
@@ -6,30 +7,58 @@ import { connect } from 'react-redux';
 
 import { Dispatch, RootState } from '../../store/store';
 import { NavigationButton, StyledSideNavigation } from './SideNavigation.styles';
+import { NavigateFunction, useLocation, useNavigate } from 'react-router-dom';
+
+type NavigationButtonProps = {
+  icon: any;
+  name: string;
+  navigate: NavigateFunction;
+  pathname: string;
+  route: string;
+  tooltipTitle: string;
+}
+
+const renderNavigationButton = ({ icon, name, navigate, pathname, route, tooltipTitle }: NavigationButtonProps) => {
+  const active = pathname === route;
+
+  const handlePageSelection = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!active) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      route && navigate(route);
+    }
+  }
+
+  return (
+    <Tooltip title={tooltipTitle} placement="right" arrow>
+      <NavigationButton name={name} onClick={handlePageSelection}>
+        {icon}
+      </NavigationButton>
+    </Tooltip>
+  )
+}
 
 interface SideNavigationProps extends SideNavigationConnect {
 
 }
 
 const SideNavigation: React.FC<SideNavigationProps> = (props) => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
   const { signOut } = props;
+
   return (
     <StyledSideNavigation>
 
       <div className="navigation-main">
-        <Tooltip title="Portfolios" placement="right" arrow>
-          <NavigationButton>
-            <DataSaverOffIcon />
-          </NavigationButton>
-        </Tooltip>
+        {renderNavigationButton({ icon: <ShowChartIcon />, name: 'home', navigate, pathname, route: '/', tooltipTitle: 'Home' })}
+        {renderNavigationButton({ icon: <DataSaverOffIcon />, name: 'portfolios', navigate, pathname, route: '/portfolios', tooltipTitle: 'Portfolios' })}
       </div>
 
       <div className="navigation-user">
-        <Tooltip title="My Account" placement="right" arrow>
-          <NavigationButton>
-            <PersonOutlineIcon fontSize="medium" />
-          </NavigationButton>
-        </Tooltip>
+        {renderNavigationButton({ icon: <PersonOutlineIcon fontSize="medium" />, name: 'userAccount', navigate, pathname, route: '/user', tooltipTitle: 'My Account' })}
 
         <Tooltip title="Sign Out" placement="right" arrow>
           <NavigationButton onClick={signOut}>
