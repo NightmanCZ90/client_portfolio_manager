c0963482e2a470dbbbd1ac0ab3460fc9b53cddb8
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-10 00:31:50 +0200

    feat: Implement portfolio model and PortfolioCards to Portfolio page

---
+++ src/pages/Portfolios/Portfolios.styles.ts
@@ -0,0 +1,47 @@
+import { styled } from '@mui/system';
+
+export const StyledPortfolios = styled('div')`
+  width: 100%;
+`;
+
+export const StyledPortfoliosHeader = styled('div')`
+  height: 10rem;
+  padding: 3rem 6rem 0 6rem;
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--color-header);
+
+  .header-info {
+    p {
+      color: var(--color-header-subtitle);
+    }
+  }
+
+  .header-user {
+    p {
+      color: var(--color-header-subinfo);
+    }
+  }
+`;
+
+export const StyledPortfoliosContent = styled('div')`
+  padding: 3rem 6rem;
+
+  .buttons-wrapper {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 2rem;
+  }
+
+  .portfolio-personal,
+  .portfolio-managed {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2rem;
+
+    .portfolio-cards {
+      gap: 1.5rem;
+      margin: 1.5rem 0;
+    }
+  }
+`;
--- src/pages/Portfolios/Portfolios.tsx
+++ src/pages/Portfolios/Portfolios.tsx
@@ -1,8 +1,88 @@
+import AddIcon from '@mui/icons-material/Add';
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PortfolioCard from '../../components/PortfolioCard';
 
-const Portfolios: React.FC = () => {
+import { PrimaryButton } from '../../constants/components';
+import { Dispatch, RootState } from '../../store/store';
+import { Portfolio } from '../../types/portfolio';
+import { StyledPortfolios, StyledPortfoliosContent, StyledPortfoliosHeader } from './Portfolios.styles';
+
+interface PortfoliosProps extends PortfoliosConnect {
+
+}
+
+const Portfolios: React.FC<PortfoliosProps> = (props) => {
+
+  const { currentUser, getPortfolios, loading, personalPortfolios, managedPortfolios } = props;
+
+  useEffect(() => {
+    (async () => {
+      await getPortfolios();
+    })();
+  }, []);
+
+  if (!currentUser) return null;
+
+  const renderPortfolioCards = (portfolios: Portfolio[]) => {
+    if (portfolios.length === 0) {
+      return <div>No Portfolios</div>
+    }
+
+    // TODO: split into two groups by investor and pm
+    return portfolios.map(portfolio => (
+      <PortfolioCard
+        portfolio={portfolio}
+        currency={currentUser.currency}
+      />
+    ))
+  }
+
   return (
-    <div>Portfolios</div>
+    <StyledPortfolios>
+      <StyledPortfoliosHeader>
+        <div className="header-info">
+          <h2>Your Portfolios</h2>
+          <p>Create and update your portfolios here</p>
+        </div>
+        <div className="header-user">
+          <h2>Your portfolios value</h2>
+          <p>13.000 $</p>
+        </div>
+      </StyledPortfoliosHeader>
+      <StyledPortfoliosContent>
+        <div className="buttons-wrapper">
+          <PrimaryButton startIcon={<AddIcon />}>Add Portfolio</PrimaryButton>
+        </div>
+        <div className="portfolio-personal">
+          <h2>Personal portfolios</h2>
+          <div className="portfolio-cards">
+            {loading ? 'loading' : renderPortfolioCards(personalPortfolios)}
+          </div>
+        </div>
+        <div className="portfolio-managed">
+          <h2>Managed portfolios</h2>
+          <div className="portfolio-cards">
+            {loading ? 'loading' : renderPortfolioCards(managedPortfolios)}
+          </div>
+        </div>
+      </StyledPortfoliosContent>
+    </StyledPortfolios>
   )
 }
 
-export default Portfolios;
+type PortfoliosConnect = ReturnType<typeof mapState> & ReturnType<typeof mapDispatch>;
+
+const mapState = (state: RootState) => ({
+  error: state.portfolios.error,
+  loading: state.portfolios.loading,
+  personalPortfolios: state.portfolios.personal,
+  managedPortfolios: state.portfolios.managed,
+  currentUser: state.currentUser.user,
+});
+
+const mapDispatch = (dispatch: Dispatch) => ({
+  getPortfolios: dispatch.portfolios.getPortfolios,
+});
+
+export default connect(mapState, mapDispatch)(Portfolios);
