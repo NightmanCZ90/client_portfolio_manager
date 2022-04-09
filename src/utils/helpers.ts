4aaaee630a11e84130c251c34d549912bf4af061
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-10 00:27:32 +0200

    data: Add function to return color based on profit or loss

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,3 +2,9 @@ export const capitalizeFirst = (str: string) => {
   if (str.length === 0) return '';
   return str[0].toUpperCase() + str.slice(1);
 }
+
+export const generateGreenRedClass = (value: number) => {
+  if (value > 0) return 'green';
+  if (value < 0) return 'red';
+  return '';
+}
