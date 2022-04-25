a5b2b56ca682065f6b9f9052d0512b1876bbdcd6
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-25 22:27:52 +0200

    feat: Add validation and form to CreateTransaction component

--- src/components/CreateTransaction/CreateTransaction.styles.ts
+++ src/components/CreateTransaction/CreateTransaction.styles.ts
@@ -1,14 +1,17 @@
 import { styled } from '@mui/system';
 
 export const StyledCreateTransaction = styled('div')`
-  padding: 1rem 0;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
 
-  .required,
-  .defaulted {
+  form {
+    padding: 1rem 0;
     display: flex;
+    flex-direction: column;
     gap: 1rem;
+
+    .required,
+    .defaulted {
+      display: flex;
+      gap: 1rem;
+    }
   }
 `;
--- src/components/CreateTransaction/CreateTransaction.tsx
+++ src/components/CreateTransaction/CreateTransaction.tsx
@@ -1,7 +1,8 @@
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, ToggleButtonGroup } from '@mui/material';
 import { useState } from 'react';
-import { CustomToggleButton } from '../../constants/components';
 
+import { CustomToggleButton, PrimaryButton } from '../../constants/components';
+import { createTransactionFormSchema } from '../../constants/validations';
 import { ExecutionType, TransactionType } from '../../types/transaction';
 import { StyledCreateTransaction } from './CreateTransaction.styles'
 
@@ -14,7 +15,7 @@ const initialFormData = {
   price: 1, // 0.0001
   currency: 'USD',
   execution: ExecutionType.FIFO,
-  commissions: null,
+  commissions: 0,
   notes: '',
 }
 
@@ -79,6 +80,16 @@ const CreateTransaction: React.FC<CreateTransactionProps> = (props) => {
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
 
     // TODO: Implement validation
+    createTransactionFormSchema(event.target.name).validate({ [event.target.name]: event.target.value })
+    .then((value) => {
+      setFormDataErrors({ ...formDataErrors, [event.target.name]: '' });
+    })
+    .catch((err) => {
+      setFormDataErrors({ ...formDataErrors, [event.target.name]: err.message });
+      if ((event.target.name === 'numShares' || event.target.name === 'price' || event.target.name === 'commissions') && event.target.value === '') {
+        setFormDataErrors({ ...formDataErrors, [event.target.name]: '' });
+      }
+    });
 
     setFormData({
       ...formData,
@@ -86,137 +97,149 @@ const CreateTransaction: React.FC<CreateTransactionProps> = (props) => {
     });
   }
 
+  const handleSubmit = () => {
+
+  }
+
   return (
     <StyledCreateTransaction>
 
-      <div className="transaction-type">
-        <ToggleButtonGroup
-          color="primary"
-          value={formData.type}
-          exclusive
-          onChange={handleTransactionTypeSelection}
-        >
-          <CustomToggleButton value={TransactionType.Buy}>Buy</CustomToggleButton>
-          <CustomToggleButton value={TransactionType.Sell}>Sell</CustomToggleButton>
-          <CustomToggleButton disabled value={TransactionType.BuyToCover}>Buy to cover</CustomToggleButton>
-          <CustomToggleButton disabled value={TransactionType.DRIP}>Drip</CustomToggleButton>
-          <CustomToggleButton disabled value={TransactionType.Dividends}>Dividends</CustomToggleButton>
-          <CustomToggleButton disabled value={TransactionType.Split}>Split</CustomToggleButton>
-        </ToggleButtonGroup>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="transaction-type">
+          <ToggleButtonGroup
+            color="primary"
+            value={formData.type}
+            exclusive
+            onChange={handleTransactionTypeSelection}
+          >
+            <CustomToggleButton value={TransactionType.Buy}>Buy</CustomToggleButton>
+            <CustomToggleButton value={TransactionType.Sell}>Sell</CustomToggleButton>
+            <CustomToggleButton disabled value={TransactionType.BuyToCover}>Buy to cover</CustomToggleButton>
+            <CustomToggleButton disabled value={TransactionType.DRIP}>Drip</CustomToggleButton>
+            <CustomToggleButton disabled value={TransactionType.Dividends}>Dividends</CustomToggleButton>
+            <CustomToggleButton disabled value={TransactionType.Split}>Split</CustomToggleButton>
+          </ToggleButtonGroup>
+        </div>
 
-      <div className="required">
-        {/* TODO: Implement search to find stock symbol after API service implementation */}
-        <TextField
-          required
-          id="stock-symbol-input"
-          label="Stock symbol"
-          name="symbol"
-          value={formData.symbol}
-          error={Boolean(formDataErrors.symbol)}
-          helperText={formDataErrors.symbol}
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          id="transaction-time-input"
-          label="Transaction time"
-          type="date"
-          name="time"
-          value={formData.time}
-          error={Boolean(formDataErrors.time)}
-          helperText={formDataErrors.time}
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          id="shares-number-input"
-          label="Share amount"
-          type="number"
-          name="numShares"
-          inputProps={{ min: 0 }}
-          value={formData.numShares}
-          error={Boolean(formDataErrors.numShares)}
-          helperText={formDataErrors.numShares}
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          id="stock-price-input"
-          label="Stock price"
-          type="number"
-          name="price"
-          inputProps={{ min: 0 }}
-          value={formData.price}
-          error={Boolean(formDataErrors.price)}
-          helperText={formDataErrors.price}
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          id="stock-currency-input"
-          label="Currency"
-          name="currency"
-          value={formData.currency}
-          error={Boolean(formDataErrors.currency)}
-          helperText={formDataErrors.currency}
-          onChange={handleChange}
-        />
-      </div>
+        <div className="required">
+          {/* TODO: Implement search to find stock symbol after API service implementation */}
+          <TextField
+            required
+            id="stock-symbol-input"
+            label="Stock symbol"
+            name="symbol"
+            value={formData.symbol}
+            error={Boolean(formDataErrors.symbol)}
+            helperText={formDataErrors.symbol}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="transaction-time-input"
+            label="Transaction time"
+            type="date"
+            name="time"
+            value={formData.time}
+            error={Boolean(formDataErrors.time)}
+            helperText={formDataErrors.time}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="shares-number-input"
+            label="Share amount"
+            type="number"
+            name="numShares"
+            inputProps={{ min: 0 }}
+            value={formData.numShares}
+            error={Boolean(formDataErrors.numShares)}
+            helperText={formDataErrors.numShares}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="stock-price-input"
+            label="Stock price"
+            type="number"
+            name="price"
+            inputProps={{ min: 0 }}
+            value={formData.price}
+            error={Boolean(formDataErrors.price)}
+            helperText={formDataErrors.price}
+            onChange={handleChange}
+          />
+          <TextField
+            required
+            id="stock-currency-input"
+            label="Currency"
+            name="currency"
+            value={formData.currency}
+            error={Boolean(formDataErrors.currency)}
+            helperText={formDataErrors.currency}
+            onChange={handleChange}
+          />
+        </div>
 
-      <div className="defaulted">
+        <div className="defaulted">
+          <TextField
+            id="transaction-commissions-input"
+            label="Commissions"
+            type="number"
+            name="commissions"
+            value={formData.commissions}
+            error={Boolean(formDataErrors.commissions)}
+            helperText={formDataErrors.commissions}
+            onChange={handleChange}
+          />
+          {formData.type === TransactionType.Sell ? (
+            <FormControl required={formData.type === TransactionType.Sell}>
+              <InputLabel id="execution-type">Execution</InputLabel>
+              <Select
+                labelId="execution-type"
+                id="stock-execution-type-input"
+                value={formData.execution}
+                label="Execution"
+                onChange={handleExecutionSelection}
+              >
+                <MenuItem value={ExecutionType.FIFO}>
+                  {executionTypes[ExecutionType.FIFO]}
+                </MenuItem>
+                <MenuItem value={ExecutionType.LIFO}>
+                  {executionTypes[ExecutionType.LIFO]}
+                </MenuItem>
+                <MenuItem value={ExecutionType.WeightedAverage}>
+                  {executionTypes[ExecutionType.WeightedAverage]}
+                </MenuItem>
+                <MenuItem value={ExecutionType.SpecificLots}>
+                  {executionTypes[ExecutionType.SpecificLots]}
+                </MenuItem>
+                <MenuItem value={ExecutionType.HighCost}>
+                  {executionTypes[ExecutionType.HighCost]}
+                </MenuItem>
+                <MenuItem value={ExecutionType.LowCost}>
+                  {executionTypes[ExecutionType.LowCost]}
+                </MenuItem>
+              </Select>
+            </FormControl>
+          ) : null}
+        </div>
         <TextField
-          id="transaction-commissions-input"
-          label="Commissions"
-          name="commissions"
-          value={formData.commissions}
-          error={Boolean(formDataErrors.commissions)}
-          helperText={formDataErrors.commissions}
+          fullWidth
+          multiline
+          id="transaction-notes-input"
+          label="Notes"
+          name="notes"
+          value={formData.notes}
+          error={Boolean(formDataErrors.notes)}
+          helperText={formDataErrors.notes}
           onChange={handleChange}
         />
-        {formData.type === TransactionType.Sell ? (
-          <FormControl required={formData.type === TransactionType.Sell}>
-            <InputLabel id="execution-type">Execution</InputLabel>
-            <Select
-              labelId="execution-type"
-              id="stock-execution-type-input"
-              value={formData.execution}
-              label="Execution"
-              onChange={handleExecutionSelection}
-            >
-              <MenuItem value={ExecutionType.FIFO}>
-                {executionTypes[ExecutionType.FIFO]}
-              </MenuItem>
-              <MenuItem value={ExecutionType.LIFO}>
-                {executionTypes[ExecutionType.LIFO]}
-              </MenuItem>
-              <MenuItem value={ExecutionType.WeightedAverage}>
-                {executionTypes[ExecutionType.WeightedAverage]}
-              </MenuItem>
-              <MenuItem value={ExecutionType.SpecificLots}>
-                {executionTypes[ExecutionType.SpecificLots]}
-              </MenuItem>
-              <MenuItem value={ExecutionType.HighCost}>
-                {executionTypes[ExecutionType.HighCost]}
-              </MenuItem>
-              <MenuItem value={ExecutionType.LowCost}>
-                {executionTypes[ExecutionType.LowCost]}
-              </MenuItem>
-            </Select>
-          </FormControl>
-        ) : null}
-      </div>
-      <TextField
-        fullWidth
-        multiline
-        id="transaction-notes-input"
-        label="Notes"
-        name="notes"
-        value={formData.notes}
-        error={Boolean(formDataErrors.notes)}
-        helperText={formDataErrors.notes}
-        onChange={handleChange}
-      />
+
+        <PrimaryButton type="submit">
+          Create Transaction
+        </PrimaryButton>
+      </form>
+
     </StyledCreateTransaction>
   )
 }
