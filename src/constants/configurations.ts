4ee643a6d62e29630fce6c2174d7ff0d9fcaeca8
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-26 22:57:35 +0200

    data: Update numShares decimals for USD

--- src/constants/configurations.ts
+++ src/constants/configurations.ts
@@ -2,7 +2,7 @@ import { CurrencyDecimals } from '../types/utility';
 
 export const fixedDecimals: CurrencyDecimals = {
   USD: {
-    numShares: 2,
+    numShares: 12,
     price: 4,
     commissions: 2,
   }
