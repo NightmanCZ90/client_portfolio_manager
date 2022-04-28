3c908a1f91267ef556e815bee027d860df6ff724
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-28 23:22:44 +0200

    feat: Add basic structure for Transaction card

---
+++ src/components/TransactionCard/TransactionCard.styles.ts
@@ -0,0 +1,13 @@
+import { styled } from '@mui/system';
+
+export const StyledTransactionCard = styled('div')`
+  width: 100%;
+  padding: 1rem;
+  border-radius: 0.5rem;
+
+  &:hover {
+    background-color: var(--primary-background);
+    opacity: 0.3;
+  }
+`;
+
--- src/components/TransactionCard/TransactionCard.tsx
+++ src/components/TransactionCard/TransactionCard.tsx
@@ -1,8 +1,30 @@
-const TransactionCard: React.FC = () => {
+import { Transaction, TransactionType } from '../../types/transaction';
+import { StyledTransactionCard } from './TransactionCard.styles';
+
+interface TransactionCardProps {
+  transaction: Transaction
+}
+
+const TransactionCard: React.FC<TransactionCardProps> = (props) => {
+  const { transaction } = props;
+
+  const calculateTransactionCharge = () => {
+    const result = transaction.numShares * transaction.price;
+    if (transaction.transactionType === TransactionType.Buy) return -1 * result;
+    if (transaction.transactionType === TransactionType.Sell) return 1 * result;
+  }
+
   return (
-    <div>
-      TransactionCard
-    </div>
+    <StyledTransactionCard>
+      <div>
+        {transaction.transactionType}
+        {calculateTransactionCharge()}
+      </div>
+      <div>
+        {transaction.numShares}
+        {transaction.price}
+      </div>
+    </StyledTransactionCard>
   )
 }
 
