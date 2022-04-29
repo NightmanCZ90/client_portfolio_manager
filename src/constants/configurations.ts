b6c21e94fec434535cdbf752cd9cf05eba3b47f3
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-29 17:00:25 +0200

    fix: Fix num shares configuration decimals

--- src/constants/configurations.ts
+++ src/constants/configurations.ts
@@ -2,7 +2,7 @@ import { CurrencyDecimals } from '../types/utility';
 
 export const fixedDecimals: CurrencyDecimals = {
   USD: {
-    numShares: 12,
+    numShares: 4,
     price: 4,
     commissions: 2,
   }
