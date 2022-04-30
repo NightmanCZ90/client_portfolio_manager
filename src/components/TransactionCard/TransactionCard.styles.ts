b087987dd58ac6f79bd45462c619f360f06597c7
NightmanCZ90 <novak.jan90@gmail.com>
2022-04-30 23:27:39 +0200

    feat: Add transaction info and styles to TransactionCard component

--- src/components/TransactionCard/TransactionCard.styles.ts
+++ src/components/TransactionCard/TransactionCard.styles.ts
@@ -4,10 +4,61 @@ export const StyledTransactionCard = styled('div')`
   width: 100%;
   padding: 1rem;
   border-radius: 0.5rem;
+  display: flex;
+  background-color: var(--card-hover-verylight);
+
+  .icon {
+    display: flex;
+    align-items: center;
+    margin-right: 1rem;
+
+    &.buy {
+      color: var(--text-success);
+    }
+
+    &.sell {
+      color: var(--text-error);
+    }
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    .top {
+      display: flex;
+      justify-content: space-between;
+
+      h3 {
+        display: inline-block;
+        margin-left: 1rem;
+      }
+    }
+
+    .middle {
+      display: flex;
+      justify-content: space-between;
+
+      .info {
+        display: flex;
+        flex-direction: row;
+        .num-shares {
+          margin-right: 1rem;
+        }
+      }
+
+
+      .date {
+        align-self: flex-end;
+      }
+    }
+  }
+
 
   &:hover {
-    background-color: var(--primary-background);
-    opacity: 0.3;
+    background-color: var(--card-hover-light);
+    /* opacity: 0.3; */
   }
 `;
 
--- src/components/TransactionCard/TransactionCard.tsx
+++ src/components/TransactionCard/TransactionCard.tsx
@@ -1,3 +1,6 @@
+import UploadIcon from '@mui/icons-material/Upload';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+
 import { Transaction, TransactionType } from '../../types/transaction';
 import { StyledTransactionCard } from './TransactionCard.styles';
 
@@ -8,6 +11,16 @@ interface TransactionCardProps {
 const TransactionCard: React.FC<TransactionCardProps> = (props) => {
   const { transaction } = props;
 
+  const getClass = () => {
+    if (transaction.transactionType === TransactionType.Buy) return 'buy';
+    if (transaction.transactionType === TransactionType.Sell) return 'sell';
+  }
+
+  const renderIcon = () => {
+    if (transaction.transactionType === TransactionType.Buy) return <FileDownloadIcon fontSize="large" />;
+    if (transaction.transactionType === TransactionType.Sell) return <UploadIcon fontSize="large" />;
+  }
+
   const calculateTransactionCharge = () => {
     const result = transaction.numShares * transaction.price;
     if (transaction.transactionType === TransactionType.Buy) return -1 * result;
@@ -16,13 +29,36 @@ const TransactionCard: React.FC<TransactionCardProps> = (props) => {
 
   return (
     <StyledTransactionCard>
-      <div>
-        {transaction.transactionType}
-        {calculateTransactionCharge()}
+      <div className={`icon ${getClass()}`}>
+        {renderIcon()}
       </div>
-      <div>
-        {transaction.numShares}
-        {transaction.price}
+      <div className="content">
+        <div className="top">
+          <div className="type-name">
+            {transaction.transactionType.toUpperCase()}
+            <h3>{transaction.stockName}</h3>
+          </div>
+          {calculateTransactionCharge()}
+          &nbsp;
+          {transaction.currency}
+        </div>
+        <div className="middle">
+          <div className="info">
+            <div className="num-shares">
+              {transaction.numShares}
+              &nbsp;
+              shares
+            </div>
+            <div className="price">
+              {transaction.price}
+              &nbsp;
+              {transaction.currency}
+            </div>
+          </div>
+          <div className="date">
+            {new Date(transaction.transactionTime).toLocaleDateString()}
+          </div>
+        </div>
       </div>
     </StyledTransactionCard>
   )
